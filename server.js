0ab73f2a73586cf849e3d6f16b8c7566f2a0adbe
Josh Lazarte <joshualazarte@yahoo.com>
2019-08-13 14:42:18 +0000

    setup working dev environment for aws c9

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
